32df02f02a8c12375f2ff404299e12770adf637d
Zlang HD <126128932+zlanghd@users.noreply.github.com>
2024-01-27 07:52:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -124,5 +124,22 @@ bot.on('messageCreate', async (msg) => {
     msg.delete().then(() => msg.channel.send({ content: `${result.translated}` }))
   }
 })
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`
+    <html>
+      <head>
+        <title>Your Web View</title>
+      </head>
+      <body style="margin: 0; padding: 0;">
+        <iframe width="100%" height="100%" src="https://axocoder.vercel.app/" frameborder="0" allowfullscreen></iframe>
+      </body>
+    </html>`);
+});
+
+server.listen(3000, () => {
+  console.log('Server Online because of Axo Coder ✅!!');
+});
 bot.login(process.env.token) // Login To The User
